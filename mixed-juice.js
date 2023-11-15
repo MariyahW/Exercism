7ddb8fcffe6cdeb960efee671d0aa8f4c7b0e4d3
MariyahW <149527258+MariyahW@users.noreply.github.com>
2023-11-14 22:05:40 -0500

    mixed-juice working

--- mixed-juice.js
+++ mixed-juice.js
@@ -10,7 +10,7 @@
  * @param {string} name
  * @returns {number} time in minutes
  */
-export function timeToMixJuice(name) {
+function timeToMixJuice(name) {
   switch (name) {
     case "Pure Strawberry Joy":
       return 0.5;
@@ -30,18 +30,6 @@ export function timeToMixJuice(name) {
     default:
       return 2.5;
   }
-
-  // if(name== "Pure Strawberry Joy"){
-  //     return .5;
-  //   }else if(name=='Energizer' ||name== 'Green Garden'){
-  //     return 1.5;
-  //   }else if(name=='Tropical Island'){
-  //     return 3;
-  //   }else if(name=='All or Nothing'){
-  //     return 5;
-  //   }else{
-  //     return 2.5;
-  //   }
 }
 
 /**
@@ -52,54 +40,61 @@ export function timeToMixJuice(name) {
  * @param {string[]} limes
  * @returns {number} number of limes cut
  */
-export function limesToCut(wedgesNeeded, limes) {
+function limesToCut(wedgesNeeded, limes) {
   let wedge = wedgesNeeded;
+
   let limeCount = 0;
   let count = 0;
-  let len = limes.length;
+  // let limesThread= limes;
 
-  while (wedge >= 0) {
-    switch (limes[count]) {
+  while (wedge >= 0 && limes.length>count) {
+    let size = limes[count];
+    switch (size) {
       case "small":
-        wedge -= 6;
+        wedge = wedge - 6;
         limeCount++;
         break;
       case "medium":
-        wedge -= 8;
+        wedge = wedge - 8;
         limeCount++;
         break;
       case "large":
-        wedge -= 0;
+        wedge = wedge - 10;
         limeCount++;
         break;
     }
-    count++;
+
     console.log(wedge);
+    // console.log(limeCount);
+    count++;
   }
+
   return limeCount;
 }
 
-/**
+/*
  * Determines which juices still need to be prepared after the end of the shift.
  *
  * @param {number} timeLeft
  * @param {string[]} orders
  * @returns {string[]} remaining orders after the time is up
  */
-export function remainingOrders(timeLeft, orders) {
+function remainingOrders(timeLeft, orders) {
   let time = timeLeft;
   let count = 0;
   let returnArray = orders;
   let len = orders.length;
-  while (0 <= time) {
-    let juice = timeToMixJuice(returnArray[count]);
+  while (time > 0) {
+    let juice = timeToMixJuice(returnArray[0]);
 
     time = time - juice;
 
     returnArray.shift();
-
-    count++;
-    console.log(time);
+    console.log(juice);
   }
+  console.log(returnArray);
   return returnArray;
 }
+
+let limes = ["small", "large", "medium", "small"];
+let limescut = limesToCut(42, limes);

efbb7bd5b7424a499530984167ed489fcdd56658
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-08-13 15:17:39 -0400

    complete task

--- c#/cars.cs
+++ c#/cars.cs
@@ -4,39 +4,43 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-       switch(speed){
-        case 0:
-            return 0;
-            break;
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-            return 100;
-            break;
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-            return 90;
-            break;
-        case 9:
-            return 88;
-            break;
-        case 10:
-            return 77;
-            break;
-        
-       }
+        switch (speed)
+        {
+            case 0:
+                return 0;
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                return 1.00;
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                return .90;
+                break;
+            case 9:
+                return .80;
+                break;
+            case 10:
+                return .77;
+                break;
+            default:
+                return .77;
+                break;
+
+        }
     }
-    
+
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        return ((speed*221)*SuccessRate(speed));
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+       return ((int)ProductionRatePerHour(speed)/60);
     }
 }
